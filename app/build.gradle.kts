232ab57ec83a7f2f71154a15659f2904eb12a76a
sogdian404 <sogdian404@gmail.com>
2026-03-22 20:22:25 +0500

    initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,18 +38,14 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-
-        //мое
-            //room
+    //room
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
@@ -60,12 +56,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     // Lifecycle (Для работы с экранами)
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-
     // Picasso - для загрузки картинок из интернета
     implementation ("com.squareup.picasso:picasso:2.71828")
-        //Retrofit
+    //Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-        //ViewPager2
+    //ViewPager2
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
 }
